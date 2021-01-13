a0b2bb34995d198b531748f1ce2a9feeaabe585e
camposleyba <martcamposleyba@gmail.com>
2021-01-13 00:25:28 -0300

    Our first commit

---
+++ generator/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):    
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    
+    # Este comando es para obtener del request que está haciendo el usario, los parametros que pasan en la barra
+    # el lenght que el usuario eligio del form del home
+    
+    length = int(request.GET.get('length',12)) #el 12 de la derecha es para tener un default
+    
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%&*()'))
+        
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    
+    thepassword = ''
+    for i in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'generator/password.html', {'password':thepassword})
+
+
+
